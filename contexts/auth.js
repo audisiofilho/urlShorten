80fa09198aad5f5b4a7ef7af46bac98104ae1696
audisiofilho <obuzua46@gmail.com>
2022-03-06 22:51:06 -0300

    urlShorten

---
+++ contexts/auth.js
@@ -0,0 +1,130 @@
+import { useState, createContext, useEffect } from "react";
+import { toast } from 'react-toastify'
+
+import firebase from "../services/firebaseConnection";
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    function loadStorage() {
+      const storageUser = localStorage.getItem("ShortenUser");
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+
+      setLoading(false);
+    }
+
+    loadStorage();
+  }, []);
+
+  //fazer cadastro
+  async function signUp(email, password, nome) {
+    setLoadingAuth(true);
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        await firebase
+          .firestore()
+          .collection("users")
+          .doc(uid)
+          .set({
+            nome: nome,
+            avatarUrl: null,
+          })
+          .then(() => {
+            let data = {
+              uid: uid,
+              nome: nome,
+              email: value.user.email,
+              avatarUrl: null,
+            };
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+            toast.success('Bem vindo a plataforma!');
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Ops! Algo deu errado!')
+        setLoadingAuth(false);
+      });
+  }
+
+  function storageUser(data) {
+    localStorage.setItem("ShortenUser", JSON.stringify(data));
+  }
+
+  //fazer login
+  async function signIn(email, password) {
+    setLoadingAuth(true);
+
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        const userProfile = await firebase
+          .firestore()
+          .collection("users")
+          .doc(uid)
+          .get();
+
+        let data = {
+          uid: uid,
+          nome: userProfile.data().nome,
+          avatarUrl: userProfile.data().avatarUrl,
+          email: value.user.email,
+        };
+
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+        toast.success('Bem vindo de volta!');
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Ops! Algo deu errado!')
+        setLoadingAuth(false);
+      });
+  }
+
+  //fazer logout
+  async function signOut() {
+    await firebase.auth().signOut();
+    localStorage.removeItem("ShortenUser");
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signed: !!user,
+        user,
+        loading,
+        signUp,
+        signIn,
+        signOut,
+        loadingAuth,
+        setUser,
+        storageUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
